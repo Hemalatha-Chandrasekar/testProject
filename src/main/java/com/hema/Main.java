8370645de5e0edb9803ef0b801fd9d33c5209ad8
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2025-01-27 22:53:49 -0600

    Initial Commit

--- src/main/java/com/hema/Main.java
+++ src/main/java/com/hema/Main.java
@@ -1,17 +1,75 @@
 package com.hema;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.hema.model.Department;
+import com.hema.model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class App {
+    public static void main(String[] args) {
+        manyToOne();
+        oneToMany();
+    }
+
+    public static void manyToOne() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        //creating departments
+        Department dept1 = new Department("IT");
+        Department dept2 = new Department("HR");
+
+        //creating teacher
+        Teacher t1 = new Teacher("1000", "MHaseeb", dept1);
+        Teacher t2 = new Teacher("2220", "Shahparan", dept1);
+        Teacher t3 = new Teacher("3000", "James", dept1);
+        Teacher t4 = new Teacher("40000", "Joseph", dept2);
+
+        //Storing Departments in database
+        session.persist(dept1);
+        session.persist(dept2);
+        //Storing teachers in database
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
+        session.persist(t4);
+        transaction.commit();
+        factory.close();
+    }
+    public static void oneToMany(){
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        //creating teacher
+        Teacher t1 = new Teacher("1000","MHaseeb");
+        Teacher t2 = new Teacher("2220", "Shahparan");
+        Teacher t3 = new Teacher("3000","James");
+        Teacher t4 = new Teacher("40000","Joseph");
+        Teacher t5 = new Teacher("200","Ali");
+        //Add teacher entity object to the list
+        ArrayList<Teacher> teachersList = new ArrayList<>();
+        teachersList.add(t1);
+        teachersList.add(t2);
+        teachersList.add(t3);
+        teachersList.add(t4);
+        teachersList.add(t5);
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
+        session.persist(t4);
+        session.persist(t5);
+        //Creating Department
+        Department department = new Department();
+        department.setDeptName("Development");
+        department.setTeacherList(teachersList);
+        //Storing Department
+        session.persist (department);
+        t.commit();
+        session.close();
+        factory.close();
     }
 }
